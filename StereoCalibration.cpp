8188d27b6a1a6d910b3576972dfaaafeb7e114b5
Steven Lovegrove <stevenlovegrove@gmail.com>
2012-06-15 13:20:37 -0400

    Capture and display stereo with RPG
    Signed-off-by: Steven Lovegrove <stevenlovegrove@gmail.com>

--- StereoCalibration.cpp
+++ StereoCalibration.cpp
@@ -1,73 +1,138 @@
+#include <thread>
+#include <mutex>
+
 #include <SimpleGui/Gui.h>
 #include <SimpleGui/GetPot>
 #include <SimpleGui/GLMesh.h>
 
+#include <RPG/Devices/Camera/CameraDevice.h>
+
 using namespace std;
 using namespace Eigen;
 
-const bool  USE_SCENE_GRAPH = false;
-const char* MESH_NAME = "./Models/beatle-no-wheels-no-interior-embedded-texture.blend";
+void glDrawTexturesQuad(float t, float b, float l, float r)
+{
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0); glVertex2f(l,b);
+    glTexCoord2f(1,0); glVertex2f(r,b);
+    glTexCoord2f(1,1); glVertex2f(r,t);
+    glTexCoord2f(0,1); glVertex2f(l,t);
+    glEnd();
+}
 
 class Application
 {
 public:
     Application()
-        : window(0, 0, 1024, 768, __FILE__ ),
-          meshtest()
+        : window(0, 0, 640*2, 480, __FILE__ )
     {
         Init();
     }
 
     void Init()
     {
-        meshtest.Init(MESH_NAME);
+        // -sdir /home/slovegrove/data/CityBlock-Noisy -sf 0
+        cam.SetProperty("StartFrame",    0);
+        cam.SetProperty("DataSourceDir", "/home/slovegrove/data/CityBlock-Noisy" );
+        cam.SetProperty("Channel-0",     "left.*pgm" );
+        cam.SetProperty("Channel-1",     "right.*pgm" );
+        cam.SetProperty("NumChannels",   2 );
+        cam.InitDriver("FileReader");
 
-        if(USE_SCENE_GRAPH) {
-            window.AddChildToRoot(&meshtest);
-        }else{
-            window.AddPostRenderCallback( Application::PostRender, this);
-        }
-    }
+        // Capture first images to get dimensions
+        cam.Capture(img);
+        width = img[0].width();
+        height = img[0].height();
 
-    int Run()
-    {
-        return window.Run();
-    }
+        // Create two OpenGL textures for stereo images
+        glGenTextures(2, m_glTex);
 
-    void GlSanityTest()
-    {
-        glDisable(GL_LIGHTING);
+        // Allocate texture memory on GPU
+        glBindTexture(GL_TEXTURE_2D, m_glTex[0]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE,0);
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+        glBindTexture(GL_TEXTURE_2D, m_glTex[1]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE,0);
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
-        glClearColor (0.0, 0.0, 0.0, 0.0);
-        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glColor3f (1.0, 1.0, 1.0);
-        glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-        glBegin(GL_POLYGON);
-        glVertex3f (0.25, 0.25, 0.0);
-        glVertex3f (0.75, 0.25, 0.0);
-        glVertex3f (0.75, 0.75, 0.0);
-        glVertex3f (0.25, 0.75, 0.0);
-        glEnd();
+        window.AddPostRenderCallback( Application::PostRender, this);
     }
 
     static void PostRender(GLWindow*, void* data)
     {
         Application* self = (Application*)data;
-
-//        self->GlSanityTest();
-
-//        glScalef(0.03,0.03,0.03);
-        self->meshtest.draw();
+        self->Draw();
     }
 
+    void CameraRun()
+    {
+        while(1) {
+            imageMutex.lock();
+            cam.Capture(img);
+            imageMutex.unlock();
+
+            usleep(33000);
+        }
+    }
+
+    int Run()
+    {
+        // Run Camera Loop
+        std::thread camThread( std::bind( &Application::CameraRun, this ) );
+
+        // Run GUI
+        return window.Run();
+    }
+
+    void Draw()
+    {
+        if( img.size() >= 2 )
+        {
+            imageMutex.lock();
+            glBindTexture(GL_TEXTURE_2D, m_glTex[0]);
+            glTexSubImage2D(GL_TEXTURE_2D,0,0,0,width,height,GL_LUMINANCE,GL_UNSIGNED_BYTE,img[0].Image.data);
+            glBindTexture(GL_TEXTURE_2D, m_glTex[1]);
+            glTexSubImage2D(GL_TEXTURE_2D,0,0,0,width,height,GL_LUMINANCE,GL_UNSIGNED_BYTE,img[1].Image.data);
+            imageMutex.unlock();
+
+            // Display frames
+            glDisable(GL_LIGHTING);
+            glDisable(GL_COLOR_MATERIAL );
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+
+            glClearColor (0.0, 0.0, 0.0, 0.0);
+            glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glEnable(GL_TEXTURE_2D);
+
+            glColor3f (1.0, 1.0, 1.0);
+
+            // Draw right image
+            glBindTexture(GL_TEXTURE_2D, m_glTex[0]);
+            glDrawTexturesQuad(-1,1,-1,0);
+
+            // Draw left image
+            glBindTexture(GL_TEXTURE_2D, m_glTex[1]);
+            glDrawTexturesQuad(-1,1,0,1);
+
+            glDisable(GL_TEXTURE_2D);
+        }
+    }
+
+    std::vector<rpg::ImageWrapper> img;
+    GLuint m_glTex[2];
+    int width, height;
+    std::mutex imageMutex;
+
+    CameraDevice cam;
     GLWindow window;
-    GLMesh meshtest;
 };
 
 int main (int argc, char** argv){
