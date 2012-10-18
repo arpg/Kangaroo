5622d2c070ff11106f795aa0a026e484ea214a44
RPG <rpg@robotics.gwu.edu>
2012-10-18 14:52:21 -0400

    Bilateral filter depthmaps. Adjustable vicon offsets. Coloured point cloud with normal data.
    Signed-off-by: RPG <rpg@robotics.gwu.edu>

--- applications/MocapMultiDepthFusion.cpp
+++ applications/MocapMultiDepthFusion.cpp
@@ -21,8 +21,11 @@ const int MaxHeight = 480;
 struct Sensor {
     Sensor(std::string name, ViconConnection& viconSharedConnection, int w, int h)
         :name(name), tracker(name, viconSharedConnection), w(w), h(h),
-         vbo(pangolin::GlArrayBuffer, w,h,GL_FLOAT,4, cudaGraphicsMapFlagsWriteDiscard, GL_STREAM_DRAW )
+          vbo(pangolin::GlArrayBuffer, w,h,GL_FLOAT,4, cudaGraphicsMapFlagsWriteDiscard, GL_STREAM_DRAW ),
+          cbo(pangolin::GlArrayBuffer, w,h,GL_FLOAT,4, cudaGraphicsMapFlagsWriteDiscard, GL_STREAM_DRAW )
     {
+        u0 = w/2.0f;
+        v0 = h/2.0f;
         ifstream ifs(name + "_f_T_vc.txt");
         ifs >> fu;
         fv = fu;
@@ -33,41 +36,25 @@ struct Sensor {
         ifs >> p;
         ifs >> q;
         ifs >> r;
-        T_vs = Sophus::SE3(SceneGraph::GLCart2T(x,y,z,p,q,r));
-        cout << SceneGraph::GLT2Cart(T_vs.matrix()) << endl;
+
+        glT_wv = new SceneGraph::GLAxis(0.1);
+        glT_vs = new SceneGraph::GLMovableAxis();
+        glT_vs->SetPose(SceneGraph::GLCart2T(x,y,z,p,q,r));
+        glvbo  = new SceneGraph::GLVbo(&vbo,0, &cbo);
+        glT_wv->AddChild(glT_vs);
+        glT_vs->AddChild(glvbo);
     }
 
     std::string name;
     ViconTracking tracker;
     int w, h;
     pangolin::GlBufferCudaPtr vbo;
-    Sophus::SE3 T_ws;
-    Sophus::SE3 T_vs;
+    pangolin::GlBufferCudaPtr cbo;
     float fu,fv,u0,v0;
-};
 
-struct GLSensor : public SceneGraph::GLObject {
-    GLSensor(Sensor& sensor)
-        : GLObject(sensor.name), sensor(sensor)
-    {
-    }
-
-    void DrawCanonicalObject()
-    {
-        glPushMatrix();
-        glMultMatrixd(sensor.T_ws.matrix().data());
-
-        // Just draw axis for time being
-        SceneGraph::GLAxis::DrawAxis();
-
-        glColor3f(1,1,1);
-        pangolin::RenderVbo(sensor.vbo,sensor.w, sensor.h);
-
-        glPopMatrix();
-    }
-
-protected:
-    Sensor& sensor;
+    SceneGraph::GLAxis* glT_wv;
+    SceneGraph::GLMovableAxis* glT_vs;
+    SceneGraph::GLVbo* glvbo;
 };
 
 typedef boost::ptr_map<std::string,Sensor> SensorPtrMap;
@@ -92,7 +79,7 @@ int main( int argc, char* argv[] )
 
     SetupContainer(container, 1, 640.0f/480.0f);
     container[0].SetDrawFunction(SceneGraph::ActivateDrawFunctor(graph, s_cam))
-                .SetHandler( new pangolin::Handler3D(s_cam/*, pangolin::AxisZ*/) );
+                .SetHandler( new SceneGraph::HandlerSceneGraph(graph,s_cam, pangolin::AxisZ) );
 
     SensorPtrMap sensors;
 
@@ -100,9 +87,15 @@ int main( int argc, char* argv[] )
     std::vector<rpg::ImageWrapper> images;
 
     Gpu::Image<unsigned short, Gpu::TargetDevice, Gpu::Manage> imgd(MaxWidth,MaxHeight);
+    Gpu::Image<float,  Gpu::TargetDevice, Gpu::Manage> imgf(MaxWidth,MaxHeight);
     Gpu::Image<float4, Gpu::TargetDevice, Gpu::Manage> imgv(MaxWidth,MaxHeight);
+    Gpu::Image<float4, Gpu::TargetDevice, Gpu::Manage> imgn(MaxWidth,MaxHeight);
 
     ViconConnection vicon(ViconIp);
+
+    pangolin::Var<int> biwin("ui.size",10, 1, 20);
+    pangolin::Var<float> bigs("ui.gs",10, 1E-3, 5);
+    pangolin::Var<float> bigr("ui.gr",700, 1E-3, 200);
 
     for(unsigned long frame=0; !pangolin::ShouldQuit(); ++frame)
     {
@@ -125,27 +118,29 @@ int main( int argc, char* argv[] )
                     it = sensors.insert(sensor_name, newsensor ).first;
 
                     // add to posegraph
-                    graph.AddChild(new GLSensor(*newsensor));
+                    graph.AddChild(newsensor->glT_wv);
                 }
 
                 Sensor& sensor = *it->second;
-    //            cout << "Data from " << sensor.name << endl;
 
-                sensor.T_ws = sensor.tracker.T_wf() * sensor.T_vs;
+                sensor.glT_wv->SetPose(sensor.tracker.T_wf().matrix());
 
                 // Update depth map. (lets assume we have a kinect for now)
                 imgd.CopyFrom<Gpu::TargetHost,Gpu::DontManage>(images[ni].Image);
-                const float asusfocal = 570.342;
-                const float df = asusfocal;
+                Gpu::BilateralFilter<float,unsigned short>(imgf,imgd,bigs,bigr,biwin,200);
+                Gpu::DepthToVbo(imgv, imgf, sensor.fu, sensor.fv, sensor.u0, sensor.v0, 1.0f/1000.0f );
+                Gpu::NormalsFromVbo(imgn, imgv);
 
-                // Convert to Vertex Buffer
-                Gpu::DepthToVbo(imgv, imgd, df, df, dw/2.0f, dh/2.0f, 1.0f/1000.0f );
-
-                // Copy to sensors vbo for display
                 {
                     pangolin::CudaScopedMappedPtr var(sensor.vbo);
                     Gpu::Image<float4> dVbo((float4*)*var,dw,dh);
                     dVbo.CopyFrom(imgv);
+                }
+
+                {
+                    pangolin::CudaScopedMappedPtr var(sensor.cbo);
+                    Gpu::Image<float4> dCbo((float4*)*var,dw,dh);
+                    dCbo.CopyFrom(imgn);
                 }
             }
         }
