121a7304c99b07872aa0f16956c2059eeca2b6ee
Juan Falquez <jfalquez@gwu.edu>
2013-08-28 14:59:35 -0400

    Changed #if to #ifdef for HAVE_ASSIMP.

--- kangaroo/MarchingCubes.cpp
+++ kangaroo/MarchingCubes.cpp
@@ -25,7 +25,7 @@
 #include "MarchingCubes.h"
 #include "MarchingCubesTables.h"
 
-#if HAVE_ASSIMP
+#ifdef HAVE_ASSIMP
 #include <assimp/cexport.h>
 #include <assimp/scene.h>
 #include "extra/AssimpMissing.h"
@@ -34,7 +34,7 @@
 namespace roo
 {
 
-#if HAVE_ASSIMP
+#ifdef HAVE_ASSIMP
 
 //fGetOffset finds the approximate point of intersection of the surface
 // between two points with the values fValue1 and fValue2
@@ -221,7 +221,7 @@ void SaveMesh(std::string filename, aiMesh* mesh)
 template<typename T, typename TColor>
 void SaveMesh(std::string filename, const BoundedVolume<T,TargetHost> vol, const BoundedVolume<TColor,TargetHost> volColor )
 {
-#if HAVE_ASSIMP
+#ifdef HAVE_ASSIMP
     std::vector<aiVector3D> verts;
     std::vector<aiVector3D> norms;
     std::vector<aiFace> faces;
